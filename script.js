ff50eb0912bd0d2dc11b20969d839a69e2464cbc
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-19 07:58:07 +0000

    fixing js errors

--- script.js
+++ script.js
@@ -6,30 +6,33 @@ let str = "";
 let arr = Array.from(btn);
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
-        console.log(dell);
+        // console.log(dell);
         if (e.target.innerHTML == '=') {
             try {
                 str = str.replace(/x/g, '*');
                 str = eval(str);
                 input.value = str;
+                console.log("hi1");
             } catch {
                 input.value = "Error";
                 str = "";
+                console.log("hi2");
             }
         }
         else if (e.target.innerHTML == 'AC') {
             str = "";
             input.value = str;
-            console.log("hi1");
+            console.log("hi3");
         }
         else if (e.target.classList.contains('del') || e.target.closest('.del')) {
             str = str.substring(0, str.length-1);
             input.value = str;
+            console.log("hi4");
         }
         else {
             str += e.target.innerHTML;
             input.value = str; 
-            console.log("hi3");
+            console.log("hi5");
         }
     })
 })
