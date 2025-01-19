23df855e12289d0f51d8d9f2baaf6ec0b8a9b89e
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-19 08:46:37 +0000

    fixing all the errors in js

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ let input = document.getElementById('inputBox');
 let btn = document.querySelectorAll('button');
 let dell = document.getElementsByClassName('del');
 
+console.log("History OF YOU : ");
 let str = "";
 let arr = Array.from(btn);
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
-        let value = e.target.innerHTML;
+        let value = e.target.innerHTML.trim();
         // console.log(dell);
-        if (value == '=') {
+        if (value === '=') {
             try {
-                if (str === "" || /[+\-*/]$/.test(str))
-                {
+                // console.log(str);
+                str = str.replace(/x/g, '*');
+                if (str === "" || /[+\-*%/]$/.test(str)) {
+                    str = str.slice(0, -1);
                     input.value = "Error";
-                    str = "";
                 }
-                else 
-                {
-                    str = str.replace(/x/g, '*');
+                else {
+                    console.log(str);
                     str = eval(str);
+                    console.log('=',str);
                     input.value = str;
+                    str = "";
                 }
             } catch {
                 input.value = "Error";
@@ -31,14 +34,19 @@ arr.forEach(button => {
             input.value = str;
         }
         else if (e.target.classList.contains('del') || e.target.closest('.del')) {
-            str = str.substring(0, str.length-1); //remove the last char
+            str = str.slice(0, -1); //remove the last char
             input.value = str;
         }
         else {
-            if (/[+\-*/]$/.test(str) && /[+\-*/]/.test(value))
+            // str = str.replace(/x/g, '*');
+            if (/[x\-+%/]$/.test(str) && /[x\-+%/]/.test(value)) {
                 return;
-            str += value;
-            input.value = str;
+            }
+            else 
+            {
+                str += value;
+                input.value = str;
+            }
         }
     })
 })
